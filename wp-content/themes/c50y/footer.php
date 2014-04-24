9ab77c0012d3706b6601e4cdb7d4a282085d1fc4
jacqueline-homan <jacquelinehoman7@gmail.com>
2014-04-24 06:41:47 -0400

    removed brown footer strip and added search box for site

--- wp-content/themes/c50y/footer.php
+++ wp-content/themes/c50y/footer.php
@@ -3,15 +3,21 @@
 		?><div class="footer-holder"><?php
 			?><div class="footer-frame"><?php
 				?><div class="container">
-
-
-
 					<div class="footer-copy" >
+					  <link rel="stylesheet" href="style.css" /> 
 						<h3><h3>
-							<p></p>
-							<img src="<?php 
+							<br />
+							  <div>
+								<form method="get" action="http://search.in4h.org/search">
+							      <input type="text" name="q" alt="Search" value="" maxlength="256" size="32"/>
+							      <input type="submit" name="btnG" value="Search"/>
+							      <input type="hidden" name="as_dt" value="i"/>
+							      <input type="hidden" name="as_sitesearch" value="http://in4h.org"/>
+						        </form>
+						      </div>
+						    <img src="<?php 
 								bloginfo('siteurl');
-							?>/wp-content/themes/c50y/images/clover.png" /> 
+							?>/wp-content/themes/c50y/images/clover.png" align= "right" />
 
 					</div>
 
