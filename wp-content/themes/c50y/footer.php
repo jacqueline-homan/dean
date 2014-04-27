330c3c0fc5411f1923b8343f79fdf3411a037630
jacqueline-homan <jacquelinehoman7@gmail.com>
2014-04-27 06:22:34 -0400

    added changes to footer searchbox and other presentational elements per ticket

--- wp-content/themes/c50y/footer.php
+++ wp-content/themes/c50y/footer.php
@@ -1,25 +1,31 @@
+<link rel="stylesheet" href="style.css" />
 </div></div></div><?php
 	?><div id="footer"><?php
 		?><div class="footer-holder"><?php
 			?><div class="footer-frame"><?php
 				?><div class="container">
 					<div class="footer-copy" >
-					  <link rel="stylesheet" href="style.css" /> 
-						<h3><h3>
-							<br />
-							  <div>
-								<form method="get" action="http://search.in4h.org/search">
-							      <input type="text" name="q" alt="Search" value="" maxlength="256" size="32"/>
-							      <input type="submit" name="btnG" value="Search"/>
-							      <input type="hidden" name="as_dt" value="i"/>
-							      <input type="hidden" name="as_sitesearch" value="http://in4h.org"/>
-						        </form>
-						      </div>
-						    <img src="<?php 
-								bloginfo('siteurl');
-							?>/wp-content/themes/c50y/images/clover.png" align= "right" />
+						<h3><?php bloginfo('name');?></h3>
+						<p>Agricultural Administration Building, 615 West Street, West Lafayette, IN 47907 <br/>
+						P: (765) 494-8483 <span class="sep">|</span> F: (765) 494-5876</p>
+					</div>
+					<div class="footer-logo">
+						<img src="<?php bloginfo('template_url'); ?>/images/clover.png" />
 
 					</div>
+					<div class="footer-search">
+
+								<form action="/" method="get">
+                                <fieldset>
+                                <label for="search"><?php home_url('http://www.in4h.org/'); ?>
+                                </label>
+                                <input type="text" name="s" id="search" value="">
+                                <input type="submit" class="search-submit" value=""/>
+                                </fieldset>
+                                </form>
+						     
+					</div>
+					<div class="clear"></div>
 
 				</div><?php
 			?></div><?php
--- wp-content/themes/c50y/searchform.php
+++ wp-content/themes/c50y/searchform.php
@@ -1,9 +1,3 @@
 <?php get_search_form();
   $echo = true;
 ?>
-<form action="/" method="get">
-  <fieldset>
-    <label for="search">Search in<?php echo home_url('/'); ?></label>
-    <input type="text" name="s" id="search" value="<php the_search_query(); ?>"
-  </fieldset>
-</form>
